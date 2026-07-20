e1b7b3bf535913a6e5491e23ee1227b4f2478d7f
zmeng <zmenggitee@example.com>
2026-07-20 13:17:45 +0800

    feat:[脚本] 版本修改为1.2.0

--- push_image.sh
+++ push_image.sh
@@ -5,7 +5,7 @@ HARBOR_PROJECT="server_inspect"
 HARBOR_USER="jack"
 HARBOR_PASSWORD="Jack_2023"
 IMAGE_NAME="server_inspect_web"
-IMAGE_TAG="latest"
+IMAGE_TAG="1.2.0"
 FULL_IMAGE_NAME="${HARBOR_REGISTRY}/${HARBOR_PROJECT}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 echo "===================================="
